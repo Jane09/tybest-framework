fb44be6b2beae6d41b508524df568ef8c44447dd
zhoujian <zhoujian@newtank.cn>
2018-11-28 15:25:39 +0800

    分表分库

---
+++ framework-micros/framework-micros-docker/src/main/java/com/tybest/microservice/DockerApplication.java
@@ -0,0 +1,16 @@
+package com.tybest.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tb
+ * @date 2018/11/28 14:40
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class);
+    }
+}
--- framework-microservce/readme.md
+++
@@ -1,3 +0,0 @@
-### microservice
-    
-    k8s integrate
