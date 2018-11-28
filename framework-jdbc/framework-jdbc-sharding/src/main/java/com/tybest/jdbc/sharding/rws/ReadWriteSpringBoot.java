2866c2539f74172410f0d19d270099bbb5d68164
zhoujian <zhoujian@newtank.cn>
2018-11-28 14:02:12 +0800

    分表分库

---
+++ framework-jdbc/framework-jdbc-sharding/src/main/java/com/tybest/jdbc/sharding/rws/ReadWriteSpringBoot.java
@@ -0,0 +1,18 @@
+package com.tybest.jdbc.sharding.rws;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * @author tb
+ * @date 2018/11/28 14:01
+ */
+//@Component
+public class ReadWriteSpringBoot {
+
+//    @Resource
+//    private DataSource dataSource;
+
+}
