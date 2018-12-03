a06d33c3fa904261e9f1202e97d4f7f6ab571ca6
zhoujian <zhoujian@newtank.cn>
2018-12-03 10:15:34 +0800

    基础构建-秒杀

---
+++ framework-seckill/src/main/java/com/tybest/seckill/SeckillApplication.java
@@ -0,0 +1,24 @@
+package com.tybest.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tb
+ * @date 2018/12/3 10:04
+ */
+@SpringBootApplication
+public class SeckillApplication {
+
+    /**
+     * 1. 数据库乐观锁；
+     * 2. 基于Redis的分布式锁；
+     * 3. 基于ZooKeeper的分布式锁
+     * 4. redis 订阅监听；
+     * 5. kafka消息队列
+     * 启动前 请配置application.properties中相关redis、zk以及kafka相关地址
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class);
+    }
+}
