5b8678b7ab3c261aabe99f700e89572ad4c664de
zhoujian <zhoujian@newtank.cn>
2018-12-07 15:22:37 +0800

    基础构建-tcc

---
+++ framework-tcc/src/main/java/com/tybest/tcc/TccApplication.java
@@ -0,0 +1,16 @@
+package com.tybest.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tb
+ * @date 2018/12/7 15:22
+ */
+@SpringBootApplication
+public class TccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccApplication.class);
+    }
+}
