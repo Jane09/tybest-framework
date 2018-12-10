30346575ab5634a69fd6f0b05fa063ab2183430b
zhoujian <zhoujian@newtank.cn>
2018-12-10 17:46:48 +0800

    基础构建-tcc

--- framework-tcc/src/main/java/com/tybest/tcc/transaction/Invoker.java
+++ framework-tcc/src/main/java/com/tybest/tcc/transaction/Invoker.java
@@ -1,9 +1,13 @@
 package com.tybest.tcc.transaction;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * @author tb
  * @date 2018/12/10 17:21
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Invoker {
 
 
